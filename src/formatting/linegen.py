b99fa99e8dfe6e88ee53c2b31fa6d4780717a355
bout3fiddy <11488427+bout3fiddy@users.noreply.github.com>
2022-10-25 10:29:07 +0200

    fix: rea-add another instance of BODIES token

--- src/formatting/linegen.py
+++ src/formatting/linegen.py
@@ -146,7 +146,7 @@ class LineGenerator(Visitor[Line]):
 
     def visit_simple_stmt(self, node: Node) -> Iterator[Line]:
         """A statement without nested statements."""
-        is_body_like = node.parent and node.parent.type not in tokens.BODY
+        is_body_like = node.parent and node.parent.type not in tokens.BODIES
         if is_body_like:
             yield from self.line(+1)
             yield from self.visit_default(node)
