c24f839368daac1f6ca75fd3b2c0860b85810832
benber86 <benoitb@tutanota.de>
2022-11-02 18:04:52 +1100

    Remove unused bits in string formatting

--- src/formatting/strings.py
+++ src/formatting/strings.py
@@ -118,41 +118,15 @@ def normalize_string_quotes(s: str) -> str:
         rf"([^\\]|^)\\((?:\\\\)*){orig_quote}"
     )
     body = s[first_quote_pos + len(orig_quote) : -len(orig_quote)]
-    if "r" in prefix.casefold():
-        if unescaped_new_quote.search(body):
-            # There's at least one unescaped new_quote in this raw string
-            # so converting is impossible
-            return s
 
-        # Do not introduce or remove backslashes in raw strings
-        new_body = body
-    else:
-        # remove unnecessary escapes
-        new_body = sub_twice(escaped_new_quote, rf"\1\2{new_quote}", body)
-        if body != new_body:
-            # Consider the string without unnecessary escapes as the original
-            body = new_body
-            s = f"{prefix}{orig_quote}{body}{orig_quote}"
-        new_body = sub_twice(
-            escaped_orig_quote, rf"\1\2{orig_quote}", new_body
-        )
-        new_body = sub_twice(
-            unescaped_new_quote, rf"\1\\{new_quote}", new_body
-        )
-    if "f" in prefix.casefold():
-        matches = re.findall(
-            r"""
-            (?:(?<!\{)|^)\{  # start of the string or a non-{ followed by a single {
-                ([^{].*?)  # contents of the brackets except if begins with {{
-            \}(?:(?!\})|$)  # A } followed by end of the string or a non-}
-            """,
-            new_body,
-            re.VERBOSE,
-        )
-        for m in matches:
-            if "\\" in str(m):
-                # Do not introduce backslashes in interpolated expressions
-                return s
+    # remove unnecessary escapes
+    new_body = sub_twice(escaped_new_quote, rf"\1\2{new_quote}", body)
+    if body != new_body:
+        # Consider the string without unnecessary escapes as the original
+        body = new_body
+        s = f"{prefix}{orig_quote}{body}{orig_quote}"
+    new_body = sub_twice(escaped_orig_quote, rf"\1\2{orig_quote}", new_body)
+    new_body = sub_twice(unescaped_new_quote, rf"\1\\{new_quote}", new_body)
 
     if new_quote == '"""' and new_body[-1:] == '"':
         # edge case:
