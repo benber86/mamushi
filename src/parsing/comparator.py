256e83194cfd69659487d0650127669b56cd9b57
benber86 <benoitb@tutanota.de>
2022-11-06 16:31:43 +1100

    Use new parser, refactor separate grammar for comparison

--- src/parsing/comparator.py
+++ src/parsing/comparator.py
@@ -2,12 +2,34 @@ from lark import Token
 from lark.visitors import Transformer_InPlaceRecursive
 
 from parsing import tokens
-from parsing.parser import parse_string_to_tokenless_ast
+from lark import Lark, Tree
+from parsing.parser import PythonIndenter
+
+_plain_lark_grammar = None
 
 
 class NullifyDocstrings(Transformer_InPlaceRecursive):
     def DOCSTRING(self, args):
         return Token(type_=tokens.DOCSTRING, value="")
+
+
+def plain_grammar():
+    global _plain_lark_grammar
+    if _plain_lark_grammar is None:
+        return Lark.open_from_package(
+            "parsing",
+            "grammar.lark",
+            ("/",),
+            parser="lalr",
+            start="module",
+            postlex=PythonIndenter(),
+            keep_all_tokens=tokens,
+            maybe_placeholders=False,
+        )
+
+
+def parse_string_to_tokenless_ast(code: str) -> Tree:
+    return plain_grammar().parse(code + "\n\n")
 
 
 def compare_ast(src: str, dest: str) -> bool:
