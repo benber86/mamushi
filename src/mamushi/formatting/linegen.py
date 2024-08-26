5d328245b0639cb4a48c754d372c1ad8b79f4f8c
benny <benoitb@tutanota.de>
2024-08-27 08:23:13 +1000

    Remove extcall parens handling for now

--- src/mamushi/formatting/linegen.py
+++ src/mamushi/formatting/linegen.py
@@ -452,39 +452,6 @@ def normalize_invisible_parens(
         check_lpar = isinstance(child, Leaf) and (child.value in parens_after)
 
 
-def remove_extcall_parens(node: Node) -> None:
-    if (
-        node.children[0].type == tokens.EXTERNAL_CALL
-        and len(node.children) > 1
-    ):
-        if (
-            node.children[1].type == tokens.ATOM
-            and node.children[1].children[0].type == tokens.LPAR
-        ):
-            if maybe_make_parens_invisible_in_atom(
-                node.children[1],
-                parent=node,
-                remove_brackets_around_comma=True,
-            ):
-                wrap_in_parentheses(node, node.children[1], visible=False)
-
-            # Since await is an expression we shouldn't remove
-            # brackets in cases where this would change
-            # the AST due to operator precedence.
-            # Therefore we only aim to remove brackets around
-            # power nodes that aren't also await expressions themselves.
-            # https://peps.python.org/pep-0492/#updated-operator-precedence-table
-            # N.B. We've still removed any redundant nested brackets though :)
-            opening_bracket = cast(Leaf, node.children[1].children[0])
-            closing_bracket = cast(Leaf, node.children[1].children[-1])
-            bracket_contents = node.children[1].children[1]
-            if isinstance(bracket_contents, Node) and (
-                bracket_contents.children[0].type == tokens.EXTERNAL_CALL
-            ):
-                ensure_visible(opening_bracket)
-                ensure_visible(closing_bracket)
-
-
 def maybe_make_parens_invisible_in_atom(
     node: LN,
     parent: LN,
